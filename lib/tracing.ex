40c0b3395b75b698f8cab50ad9f9b690ab7628ae
Klaus Alfert <kal@zuehlke.com>
2015-05-05 13:09:14 +0200

    Implementierung mit tests

---
+++ lib/tracing.ex
@@ -0,0 +1,34 @@
+defmodule Tracing do
+
+  @type trace_t(a) :: {a, String.t}
+  @type debug_fun(a) :: (a -> trace_t(a))
+  @type trace_fun(a) :: (debug_fun(a) -> debug_fun(a))
+
+  def f(x), do: x*2.0
+  def g(x), do: x+3.0
+  def h(x), do: x |> g |> f # == f(g(x))
+
+  def ft(x), do: {f(x), "call f with #{x}"}
+  def gt(x), do: {g(x), "call g with #{x}"}
+  def ht(x), do: comp(&ft/1, &gt/1).(x)
+
+  @spec bind(debug_fun(a)) :: trace_fun(a) when a: var
+  def bind(tfun) do
+    fn({y, s1}) ->
+      {z, s2} = tfun.(y)
+      {z, s1 <> "." <> s2}
+    end
+  end
+
+  @spec comp(debug_fun(a), debug_fun(a)) :: debug_fun(a) when a: var
+  def comp(ft, gt) do
+    fn(x) -> x |> gt.() |> (bind ft).() end
+  end
+
+  @spec unit(a) :: trace_t(a) when a: var
+  def unit(x), do: {x, ""}
+
+  @spec lift((a -> a)) :: debug_fun(a) when a: var
+  def lift(f), do: fn(x) -> f.(x) |> unit end
+
+end
--- test/monads_test.exs
+++ test/monads_test.exs
@@ -1,7 +1,16 @@
 defmodule MonadsTest do
   use ExUnit.Case
+  import Tracing
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "h(x) == ht(x)" do
+    x = 20.0
+    ht = ht(x)
+    h = h(x)
+    assert {h, _} = ht
+  end
+
+  test "commutation digram" do
+    x = 20.0
+    assert x |> comp((lift &f/1), lift(&g/1)).() == x |> (lift &(f(g(&1)))).()
   end
 end
