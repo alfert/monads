fd33879057cb05fe9feb30b64a26a180c0b462a6
Klaus Alfert <kal@zuehlke.com>
2015-05-07 13:33:03 +0200

    testting monad laws

--- test/monads_test.exs
+++ test/monads_test.exs
@@ -14,4 +14,18 @@ defmodule MonadsTest do
     assert x |> comp((lift &f/1), lift(&g/1)).() == x |>
       (lift fn(y) -> f(g(y)) end).()
   end
+
+  test "unit is neutral under bind left" do
+    x = 20.0
+    mx = ft(x)
+    assert mx |> bind(&unit/1) == mx
+  end
+
+  test "unit is neutral under bind right" do
+    x = 20.0
+    mx = ft(x)
+    assert unit(x) |> bind(&ft/1) == mx
+  end
+
+
 end
